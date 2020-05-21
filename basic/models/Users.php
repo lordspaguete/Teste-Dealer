4bd9b98a72277131d0197ec3205120c611de79af
lordspaguete <lordspaguete@gmail.com>
2020-05-21 04:05:57 -0300

    Aplicação

---
+++ basic/models/Users.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int $active
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'active'], 'required'],
+            [['active'], 'integer'],
+            [['name'], 'string', 'max' => 300],
+            [['email'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'active' => 'Active',
+        ];
+    }
+}
---
+++ basic/views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Dealer Test</h1>
+        <p><a class="btn btn-lg btn-success" href="http://localhost/basic/web/index.php?r=users">List Users</a></p>
+        
+    </div>
+
+</div>
